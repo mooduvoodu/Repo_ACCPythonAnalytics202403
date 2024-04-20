5a03c869ab2761de38ac3b3053a21ed2d6346548
heliosarts <165414948+heliosarts@users.noreply.github.com>
2024-04-20 02:55:53 +0000

    updaed

--- Dey/Assignment3.py
+++ Dey/Assignment3.py
@@ -3,4 +3,9 @@ import pandas
 df = pandas.read_csv('churn_prediction.csv')
 
 print(df.head(10))
-print(df)
+
+print(df.shape)
+
+print(df[['age', 'gender', 'occupation']])
+
+print(df[df['customer_nw_category'] == 3])
