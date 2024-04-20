d71e0fbf957fbe03c7e9a6d16ea2added3316c81
heliosarts <165414948+heliosarts@users.noreply.github.com>
2024-04-20 03:06:11 +0000

    update

--- Dey/Assignment3.py
+++ Dey/Assignment3.py
@@ -9,3 +9,6 @@ print(df.shape)
 print(df[['age', 'gender', 'occupation']])
 
 print(df[df['customer_nw_category'] == 3])
+
+print(df[(df['current_balance'] > 5000) & (df['churn'] == 0)])
+print(df[(df['age'] >= 30) & (df['age'] <= 40) & (df['occupation'] == 'salaried')])
