46b6cc1881730f00125888f6fe987b9726f4c9e2
heliosarts <165414948+heliosarts@users.noreply.github.com>
2024-04-20 13:13:49 +0000

    updated

--- Dey/Assignment 3.py
+++ Dey/Assignment 3.py
@@ -1,1 +1,9 @@
-df = 
+import pandas as pd
+
+DF=pd.read_csv('churn_prediction.csv')
+               
+print(DF.head(10))
+print(DF.shape)
+subset=DF[(DF['current_balance'] > 5000) &( DF['occupation'] == 'self_employed')]
+print(subset.head(5))
+
