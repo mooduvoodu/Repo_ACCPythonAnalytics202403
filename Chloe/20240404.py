26053bd5586ea90f5e33b967e5890e38c30402b0
Chloe0123456 <164964526+Chloe0123456@users.noreply.github.com>
2024-04-20 13:07:23 -0500

    Update 20240404.py

--- Chloe/20240404.py
+++ Chloe/20240404.py
@@ -1,10 +1,33 @@
+import requests
 import pandas as pd
-import requests as rq 
-from polygon import RESTClient
 
-client = RESTClient() # POLYGON_API_KEY is used
-client = RESTClient("BDRYmUV3NU29r6y734HcvipzFA6GQMzt") # api_key is used
+def fetch_tickers(api_key):
+    api_url = f'https://api.polygon.io/v3/reference/tickers?type=CS&market=stocks&active=true&limit=500&apiKey={api_key}'
+    tickers = []
+    roundtripcounter = 0
 
-def fetch_tickets = requests.get('https://api.polygon.io/v3/reference/tickers?type=CS&market=stocks&active=true&limit=100&apiKey=BDRYmUV3NU29r6y734HcvipzFA6GQMzt')
+    while api_url:
+        roundtripcounter += 1
+        print(f'Round trip #{roundtripcounter}')
+        PGresponse = requests.get(api_url)
+        if PGresponse.status_code != 200:
+            raise ValueError("API request failed. Please check your API key and network connection.")
+        tickerdata = PGresponse.json()
+        tickers.extend(tickerdata['results'])
+        if tickerdata.get('next_url'):
+            api_url = tickerdata.get('next_url') + f'&apiKey={api_key}'  # Update the URL for the next request
+        else:
+            break
 
-print(fetch_tickets.text)
+    # Convert the list of tickers to a DataFrame
+    df_tickers = pd.json_normalize(tickers)
+    return df_tickers
+
+# Use your API key here
+api_key = 'BDRYmUV3NU29r6y734HcvipzFA6GQMzt'
+tickers_df = fetch_tickers(api_key)
+
+# Display the first 10 rows of the DataFrame
+print(tickers_df.head(10))
+
+tickers_df.to_csv("tickers.csv", index=False)
