22e21c4a1ecdf44ac001d9d075fedb5d87c1a264
heliosarts <165414948+heliosarts@users.noreply.github.com>
2024-05-05 17:28:48 +0000

    updated

--- Dey/Assignment5.py
+++ Dey/Assignment5.py
@@ -34,3 +34,45 @@ df_dates = pd.DataFrame(data_dim_date)
 
 # Display the DataFrames to confirm creation
 df_sales.head(), df_customers.head(), df_dates.head()
+
+#1 Basic inner join
+sales_with_customers = pd.merge(df_sales, df_customers, on='CustomerID', how='inner')
+display(sales_with_customers)
+
+#2 Left Outer Join
+sales_with_customers_left = pd.merge(df_sales, df_customers, on='CustomerID', how='left')
+display(sales_with_customers_left)
+
+#3 Right Outer Join
+customers_with_sales_right = pd.merge(df_sales, df_customers, on='CustomerID', how='right')
+display(customers_with_sales_right)
+
+#4 Full Outer Join
+full_outer_join = pd.merge(df_sales, df_customers, on='CustomerID', how='outer')
+display(full_outer_join)
+
+#5 Cross Join
+customers_products_cross = df_customers.assign(key=1).merge(df_sales.assign(key=1), on='key').drop('key', axis=1)
+display(customers_products_cross)
+
+#6 Group By
+total_sales_per_customer = df_sales.groupby('CustomerID').agg(Total_Sales=pd.NamedAgg(column='TotalPrice', aggfunc='sum'))
+display(total_sales_per_customer)
+
+#7 Summarizing Data
+summary_stats = df_sales.groupby('ProductID')['TotalPrice'].agg(['sum', 'mean', 'min', 'max'])
+display(summary_stats)
+
+#8 Window Function - Rank
+df_sales['Rank'] = df_sales.groupby('CustomerID')['TotalPrice'].rank(method='dense', ascending=False)
+display(df_sales['Rank']) 
+
+#9 Window Function - Running Total
+df_sales['Running_Total'] = df_sales.sort_values('DateID').groupby('CustomerID')['TotalPrice'].cumsum()
+display(df_sales['Running_Total'])
+
+#10 Conditional Aggregation
+sales_above_threshold = df_sales[df_sales['TotalPrice'] > 50].groupby('CustomerID').agg(Total_Sales=pd.NamedAgg(column='TotalPrice', aggfunc='sum'))
+display(sales_above_threshold)
+
+
