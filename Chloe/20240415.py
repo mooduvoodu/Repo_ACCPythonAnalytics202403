e13927e23997fcb837c286a8b23d0df50bcf6b37
Chloe0123456 <164964526+Chloe0123456@users.noreply.github.com>
2024-05-11 16:27:09 -0500

    Update 20240415.py

--- Chloe/20240415.py
+++ Chloe/20240415.py
@@ -18,4 +18,65 @@ multi_group_var = df\
 
 print(multi_group_var)
 
-multi_group_var2 = (df.groupby(['gender', 'occupation]) [['current_month_balance', 'previous_month_balance']].mean())
+multi_group_var2 = (df
+                    .groupby 
+                    (['gender', 'occupation]) 
+                    [['current_month_balance', 'previous_month_balance']]
+                              .mean()       
+                               )
+
+print(multi_group_var2)
+
+
+flat= multi_group_var2.reset_index()
+print(flat)
+
+print(df.groupby('gender')['occupation'].nunique()0
+print(df.groupby('gender')['occupation'].value_counts().reset_index())
+
+display(df.loc[df['current_month_balance'] > 10000.0])
+
+scientist = panda.DataFrame(
+  {
+    "Name": ["Rosaline Franklin", "William Gosset"],
+    "Occupation": ["Chemist", "Statitician"],
+    "Born": ["1920-07-25", "1876-06-13"],
+    "Died": ["1958-04-16", "1937-10-16"]
+    "Age": [37, 61],
+  }
+)
+
+print(scientists)
+
+scientists = pandas.DataFrame(
+  data={
+    "Occupation": ["Chemist", "Statitician"]
+    "Born": ["1920-07-25", "1876-06-13"],
+    "Died": ["1958-04-16", "1937-10-16"]
+    "Age": [37, 61],
+  },
+  index=["Rosaline Franklin", "William Gosset"],
+  columns=["Occupation", "Born", "Died", "Age"],
+)  
+
+print(scientists)
+
+ages = scientist['Age']
+print(ages)
+
+print(ages.mean())
+
+print(ages.min())
+
+print(ages.max())
+
+print(ages.std())
+
+print(ages + ages)
+
+print(ages + 100)
+
+print (ages * 2)
+
+
+    
